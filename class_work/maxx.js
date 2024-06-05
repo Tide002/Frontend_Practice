b650916e6b3b1dd89ba7dd92d4a425da7a9f0a31
Tide002 <anyinobaju1@gmail.com>
2024-06-05 09:41:35 +0100

    update

---
+++ class_work/maxx.js
@@ -0,0 +1,27 @@
+let mylocation = "vnicom";
+let color = "blue";
+var x = 22;
+var y = 20;
+ 
+// let a = 15; n m
+// a += 20;
+
+// alert(a);
+
+// let r = true;
+// let v = false;
+// let s = "signin_Successful";
+// let t = "signin_Failed"; 
+// let w = 5;
+// let u = w > 1 ? s : t;
+
+// alert(u);
+
+
+
+
+const fan ={type:"OX", color: "black", model: "OX plus"};
+
+const building = ["two_story", "duplex", "three_story", "bungalow", "storage_building", "family", "educational_building"];
+
+const bank_account = {password: "1234", name: "Olatide", telephone: "07019394509", banking_option: "mobile", nin:"2332337832"};
